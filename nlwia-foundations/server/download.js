d865fe3f3065b1a3d8d515ec98829eb9ba3cba82
ghmarconato <ghmarconato@gmail.com>
2024-04-05 11:39:08 -0300

    backend to frontend connection

--- nlwia-foundations/server/download.js
+++ nlwia-foundations/server/download.js
@@ -1,7 +1,9 @@
 import ytdl from 'ytdl-core'
 import fs from 'fs'
+import { resolve } from 'path'
+import { rejects } from 'assert'
 
-export const download = (videoId) => {
+export const download = (videoId) => new Promise((resolve,reject) => {
     const videoURL = "https://www.youtube.com/shorts/" + videoId
     console.log("Realizando download do vídeo: " + videoId)
 
@@ -10,15 +12,17 @@ export const download = (videoId) => {
     (info) => {
         const seconds = info.formats[0].approxDurationMs / 1000
 
-        if(seconds > 60){
+        if(seconds > 80){
             throw new Error("Vídeo muito longo (+60 segundos)")
         }
     })
     .on("end", () => {
         console.log("Download Finalizado.")
+        resolve()
     })
     .on("error", (error) => {
         console.log("Erro no Download:", error)
+        reject(error)
     })
     .pipe(fs.createWriteStream('./tmp/audio.mp4'))
-}
+})
---
+++ nlwia-foundations/web/form.js
@@ -0,0 +1,31 @@
+import {server} from "./server.js"
+const form = document.querySelector("#form")
+const input = document.querySelector("#url")
+const content = document.querySelector("#content")
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault()
+    content.classList.add("placeholder")
+
+    const videoURL = input.value
+
+    if(!videoURL.includes("/shorts/")){
+        return content.textContent = "Esse vídeo Não é um shorts"
+    }
+
+    const [_, params] = videoURL.split('/shorts/')
+    const [videoID] = params.split('?si')
+
+    content.textContent = "Obtendo o Texto do Áudio..."
+
+    const transcription = await server.get("/summary/" + videoID)
+
+    content.textContent = "Realizando o resumo..."
+
+    const summary =  await server.post("/summary", {
+        text: transcription.data.result,
+    })
+
+    content.textContent = summary.data.result
+    content.classList.remove("placeholder")
+})
